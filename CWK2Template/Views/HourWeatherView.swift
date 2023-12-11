f860fc734a1404a62e08c7a4c74c4fbb8c127651
Sam Clark <sam.clark48@hotmail.co.uk>
2023-12-11 13:20:06 +0000

    Updated hourly weather data in forecast tab

--- CWK2Template/Views/HourWeatherView.swift
+++ CWK2Template/Views/HourWeatherView.swift
@@ -9,26 +9,41 @@ import SwiftUI
 
 struct HourWeatherView: View {
     var current: Current
-
+    
     var body: some View {
         let formattedDate = DateFormatterUtils.formattedDateWithDay(from: TimeInterval(current.dt))
+        let weatherIcon = current.weather.first?.icon
+        let hourlyTemp = current.temp
+        let hourlyDescription = current.weather.first?.weatherDescription
+        
         VStack(alignment: .center, spacing: 5) {
             Text(formattedDate)
                 .font(.body)
                 .padding(.horizontal)
-                .padding(.vertical)
+                .padding(.top)
                 .foregroundColor(.black)
+            
+            AsyncImage(url: URL(string: "https://openweathermap.org/img/wn/\(weatherIcon ?? "").png"))
+                .frame(height: 40)
                 
-            Text("Add style & other elements")
+            Text(String(format: "%.0f\u{00B0}C", hourlyTemp))
                 .frame(width: 125)
                 .font(.body)
                 .multilineTextAlignment(.leading)
                 .lineLimit(nil) 
                 .padding(.horizontal)
-                .padding(.vertical)
+                .foregroundColor(.black)
+            
+            Text(hourlyDescription?.rawValue.capitalized ?? "")
+                .frame(width: 125)
+                .font(.body)
+                .multilineTextAlignment(.leading)
+                .lineLimit(nil)
+                .padding(.horizontal)
+                .padding(.bottom)
                 .foregroundColor(.black)
         }
-        .background(Color.cyan)
+        .background(Color.teal)
         .cornerRadius(5)    }
 }
 
--- CWK2Template/Views/WeatherForecastView.swift
+++ CWK2Template/Views/WeatherForecastView.swift
@@ -10,58 +10,62 @@ import SwiftUI
 struct WeatherForecastView: View {
     @EnvironmentObject var weatherMapViewModel: WeatherMapViewModel
     var body: some View {
-        NavigationView {
-            ScrollView{
-                VStack(alignment: .leading, spacing: 16) {
-                    if let hourlyData = weatherMapViewModel.weatherDataModel?.hourly {
-                        
-                        ScrollView(.horizontal, showsIndicators: false) {
+            NavigationView {
+                ZStack{
+                    Color(red: 176 / 255, green: 207 / 255, blue: 236 / 255)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(width: 365, height: 285)
+                        .offset(CGSize(width: 0, height: -250.0))
+                ScrollView{
+                    VStack(alignment: .leading, spacing: 16) {
+                        if let hourlyData = weatherMapViewModel.weatherDataModel?.hourly {
                             
-                            HStack(spacing: 10) {
+                            ScrollView(.horizontal, showsIndicators: false) {
                                 
-                                ForEach(hourlyData) { hour in
-                                    HourWeatherView(current: hour)
+                                HStack(spacing: 10) {
+                                    
+                                    ForEach(hourlyData) { hour in
+                                        HourWeatherView(current: hour)
+                                    }
+                                }
+                                .padding(.horizontal, 16)
+                            }
+                            .frame(height: 180)
+                        }
+                        Divider()
+                            .padding(.horizontal, 16)
+                            .padding(.vertical, 8)
+                        VStack {
+                            List {
+                                ForEach(weatherMapViewModel.weatherDataModel?.daily ?? []) { day in
+                                    DailyWeatherView(day: day)
                                 }
                             }
-                            .padding(.horizontal, 16)
+                            .listStyle(GroupedListStyle())
+                            .frame(height: 500)
                         }
-                        .frame(height: 180)
                     }
-                    Divider()
-                        .padding(.horizontal, 16)
-                        .padding(.vertical, 8)
-                    VStack {
-                        List {
-                            ForEach(weatherMapViewModel.weatherDataModel?.daily ?? []) { day in
-                                DailyWeatherView(day: day)
-                            }
-                        }
-                        .listStyle(GroupedListStyle())
-                        .frame(height: 500)
-                        //.background(Color.blue)
-                        //.opacity(0.2)
-                    }
+                    .padding(.horizontal, 16)
                 }
-                .padding(.horizontal, 16)
-            }
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    HStack {
-                        Image(systemName: "sun.min.fill")
-                        VStack{
-                            Text("Weather Forecast for \(weatherMapViewModel.city)").font(.title3)
-                                .fontWeight(.bold)
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            Image(systemName: "sun.min.fill")
+                            VStack{
+                                Text("Weather Forecast for \(weatherMapViewModel.city)").font(.title3)
+                                    .fontWeight(.bold)
+                            }
                         }
                     }
                 }
             }
         }
     }
-}
-
-struct WeatherForcastView_Previews: PreviewProvider {
-    static var previews: some View {
-        WeatherForecastView().environmentObject(WeatherMapViewModel())
+    
+    struct WeatherForcastView_Previews: PreviewProvider {
+        static var previews: some View {
+            WeatherForecastView().environmentObject(WeatherMapViewModel())
+        }
     }
 }
