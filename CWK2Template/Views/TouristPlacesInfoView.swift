44408b601c507a57f1b66ec8d26e55b41efa099d
Sam Clark <sam.clark48@hotmail.co.uk>
2023-12-26 16:07:38 +0000

    Implemented TouristPlacesInfoView

---
+++ CWK2Template/Views/TouristPlacesInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  TouristPlacesInfoView.swift
+//  CWK2Template
+//
+//  Created by Sam Clark on 26/12/2023.
+//
+
+import SwiftUI
+
+struct TouristPlacesInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TouristPlacesInfoView()
+}
