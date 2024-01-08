dcea94f07a6f97c6eef8c8797a8fbeb36a4e6a08
Sam Clark <sam.clark48@hotmail.co.uk>
2024-01-08 13:54:24 +0000

    Added justification for enhancements & minor weather data model fixes

--- CWK2Template/Views/TouristPlacesInfoView.swift
+++ CWK2Template/Views/TouristPlacesInfoView.swift
@@ -86,3 +86,35 @@ struct TouristPlacesInfoView: View {
         }
     }
 }
+
+/*
+ JUSTIFICATION FOR ADDED ENHANCEMENTS
+ ----------------------------------------
+ For the task requiring enhancements to the app, I have implemented a new view
+ called TouristPlacesInfoView which has the purpose of allowing the user to press
+ on one of the tourist attractions on the Place Map view, this will then take them
+ to a separate view where they can see some extra details on that attraction such
+ as the multiple images, a brief description and the wikipedia link. All these
+ details come from the places.json file and no additions have been made to the json file.
+ 
+ When the Place Map has attractions listed, the UI looks the same as the image for the
+ place map view that was provided in the coursework spec, with the minor addition of
+ arrows on each attraction to signify to the user that it is clickable. The Info View 
+ also has a built in 'Back' button that will return the user to the mapview.
+ 
+ Since the list of attractions are the buttons to go to this view, if the entered
+ location has no tourist attractions listed then they simply will not be able to reach
+ this view for that location. In the event that there are no images for the given
+ tourist attraction such as with New York in this program, the view will instead
+ have a grey box's that represent where images would be.
+ 
+ I believe this addition provides a useful and practical addition to the application
+ from the user perspective by allowing them to gain a extra information on the given
+ tourist attraction beyond what the map view provides.
+ 
+ I considered implementing the Wikipedia link as an actual link, however I was unsure
+ if from a coursework perspective, I should be allowing the application to move away
+ from the application and into Safari instead, even if it would have had a button
+ to easily return to the application.
+ ----------------------------------------
+ */
